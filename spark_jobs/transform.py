461827f89bda0e5dd67351da83f66b18a7588be6
holistudio <daniel.bin.lu@gmail.com>
2026-02-25 17:16:03 -0500

    aggregate ridership per station-hour and add time features

--- spark_jobs/transform.py
+++ spark_jobs/transform.py
@@ -19,3 +19,25 @@ spark = SparkSession.builder \
 df = spark.read.parquet(args.input)
 apt = spark.read.parquet(f"{PROCESSED}/apt/")
 
+
+# set transit_timestamp column data type to timestamp
+df = df.withColumn("transit_timestamp", F.to_timestamp("transit_timestamp"))
+
+# aggregate ridership per station-hour (combine MetroCard + OMNY)
+df = df.groupBy("station_complex", "transit_timestamp").agg(
+    F.sum("ridership").alias("ridership")
+)
+
+# add columns for additional time features based on the timestamp
+df = df \
+    .withColumn("hour_of_day", F.hour("transit_timestamp")) \
+    .withColumn("day_of_week",  F.dayofweek("transit_timestamp")) \
+    .withColumn("is_weekend", (F.dayofweek("transit_timestamp").isin(1,7).cast("int"))) \
+    .withColumn("month",        F.month("transit_timestamp")) \
+    .withColumn("year",         F.year("transit_timestamp")) \
+    .withColumn("time_window",
+                F.when(F.col("hour_of_day").between(0,6), "early_morning")
+                .when(F.col("hour_of_day").between(7,9), "morning_peak")
+                .when(F.col("hour_of_day").between(10,15), "midday")
+                .when(F.col("hour_of_day").between(16,19), "evening_peak")
+                .otherwise("late_night"))
